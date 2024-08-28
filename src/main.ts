686599badb208f8cffbc292f98e3072c7953dbdf
tr-haider <mhaidershahab@technologyrivers.com>
2024-08-29 00:52:09 +0500

    adding swagger conffig for vercel

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,18 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document,{
-    jsonDocumentUrl: 'swagger.json',
-    customCssUrl : CSS_URL
+  SwaggerModule.setup("swagger", app, document, {
+    customSiteTitle: "Api Docs",
+    customfavIcon: "https://avatars.githubusercontent.com/u/6936373?s=200&v=4",
+    customJs: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js",
+    ],
+    customCssUrl: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css",
+    ],
   });
   await app.listen(3000);
   
