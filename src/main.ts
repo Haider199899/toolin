be0ded20606ef26a73124ec2e3dcee1e148bbffa
tr-haider <mhaidershahab@technologyrivers.com>
2024-08-29 00:48:43 +0500

    adding swagger conffig for vercel

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 
 async function bootstrap() {
+  const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const { SwaggerModule, DocumentBuilder } = await import('@nestjs/swagger');
   const config = new DocumentBuilder()
     .setTitle('Toolin-mine')
     .setDescription('Toolin-mine API description')
@@ -16,6 +16,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document,{
     jsonDocumentUrl: 'swagger.json',
+    customCssUrl : CSS_URL
   });
   await app.listen(3000);
   
